fe8980f1e86e5d3c87ced574f723c0dbffd5f5d6
John Grey <johngrey4296@gmail.com>
2023-10-19 22:39:35 +0100

    [add]: stub for stub-generation

---
+++ blood/blood-stub.el
@@ -0,0 +1,25 @@
+;; doom-stub.el -*- mode: elisp; lexical-binding: t; -*-
+
+(llog! "Stub Generator")
+
+(defun stub-module! (group name &optional loc)
+  "create a stub module in a location
+  defaults to wherever (dir init.el)/modules/group/name
+  unless loc is provided, in which case loc/group/name is used"
+  (interactive)
+  ;; mkdir
+  ;; mk module file
+  ;; open it
+  )
+
+(defun stub-profile! ()
+  (interactive)
+
+  )
+
+(defun blood-stub-h ()
+  ""
+  (hlog! "TODO: Stub Module")
+  )
+
+(provide 'blood-stub)
