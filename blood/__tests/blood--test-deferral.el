91bcfe34b416125723dd6cc88553fa5e1287290c
John Grey <johngrey4296@gmail.com>
2025-01-01 03:09:42 +0000

    [incremental]: tests

--- blood/__tests/blood--test-deferral.el
+++ blood/__tests/blood--test-deferral.el
@@ -2,94 +2,44 @@
 ;;
 ;; General ERT tests for blood
 ;;
-(require 'f)
+(require 'buttercup)
 
-(defun blood--test-setup-deferral ()
-  (add-to-list 'load-path (f-parent default-directory))
-  (load "blood-deferral")
-  )
-
-(defun blood-test-h ()
-  (ilog! "TODO")
-  )
-
-(ert-deftest blood-test-deferral-sanity ()
-  :tags '(sanity)
-  (should (eq 1 1))
-  )
-
-(ert-deftest blood-test-deferral-setup ()
-  "Tests the rendering of `quote' symbols in `pp-to-string'."
-  (blood--test-setup-deferral)
-  (should (boundp 'blood-defer--load-queue))
-  (should (boundp 'blood-defer--load-timer))
-  (should (boundp 'blood-defer--skip-loads))
-  (should (fboundp 'blood-defer--start-h))
-  (should (fboundp 'file!))
-  (should (fboundp 'dir!))
-  (should (fboundp 'local-load!))
-  (should (fboundp 'defer!))
-  (should (fboundp 'defer-load!))
-  (should (fboundp 'defer-require!))
-  )
-
-(ert-deftest blood-test-deferral-local-load-expand ()
-  "Tests local load expands properly"
-  (should (equal (macroexpand '(local-load! "test"))
-                 '(load (file-name-concat (dir!) "test") nil 'nomessage)
-                 ))
-  )
-
-(ert-deftest blood-test-deferral-local-load-skip-expand ()
-  "tests skipping local load if blood-defer--skip-loads is set"
-  (let ((blood-defer--skip-loads t))
-    (should (equal (macroexpand '(local-load! "test"))
-                   nil))
+(describe "deferally setup"
+  (before-all
+    (require 'blood-deferral))
+  (it "is a sanity test" (expect t :to-be (not nil)))
+  (it "should have defined vars"
+    (expect (boundp 'blood-defer--load-queue) :to-be t)
+    (expect (boundp 'blood-defer--load-timer) :to-be t)
+    (expect (boundp 'blood-defer--skip-loads) :to-be t)
     )
+  (it "should have defined functions"
+    (expect (should (fboundp 'blood-defer--start-h)) :to-be t)
+    (expect (should (fboundp 'file!)) :to-be t)
+    (expect (should (fboundp 'dir!)) :to-be t)
+    (expect (should (fboundp 'local-load!)) :to-be t)
+    (expect (should (fboundp 'defer!)) :to-be t)
+    (expect (should (fboundp 'defer-load!)) :to-be t)
+    (expect (should (fboundp 'defer-require!)) :to-be t)
+    )
+
   )
 
-(ert-deftest blood-test-deferral-defer-macro-expand ()
-  "Tests the defer! macro"
-  (should (equal (macroexpand '(defer! 10 "test"))
-                 '(run-with-idle-timer 10 nil (lambda nil "test"))
-                 ))
-)
+(describe "local load macro"
+  (it "should expand paths properly"
+    (expect (macroexpand '(local-load! "test"))
+            :to-equal '(load (file-name-concat (dir!) "test") nil 'nomessage)
+            )
+    )
+  (it "should skip when appropriate"
+    (let ((blood-defer--skip-loads t))
+      (expect (macroexpand '(local-load! "test")) :to-be nil)))
 
-(ert-deftest blood-test-deferral-defer-load-expand ()
-  "check defer-load!"
-  (should nil)
   )
 
-(ert-deftest blood-test-deferral-defer-require-expand ()
-  "check defer-require"
-  (should nil)
-  )
-
-(ert-deftest blood-test-deferral-queue-pop-load ()
-  "Test popping a load instruction from the queue"
-  (should nil)
-  )
-
-(ert-deftest blood-test-deferral-queue-pop-require ()
-  "Test popping a require instruction from the queue"
-  (should nil)
-  )
-
-(ert-deftest blood-test-deferral-queue-pop-eval()
-  "Test popping an eval instruction from the queue"
-  (should nil)
-  )
-
-(ert-deftest blood-test-deferral-queue-pop-after ()
-  "Test popping an after instruction from the queue"
-  (should nil)
-  )
-
-(ert-deftest blood-test-deferral-queue-pop-unknown ()
-  "Test popping an after instruction from the queue"
-  (should nil)
-  )
-
-(ert-deftest blood-test-deferral-queue-pop-empty ()
-  "check queue-pop cancels the timer when empty"
+(describe "defer macro"
+  (it "should expand properly"
+  (expecy (macroexpand '(defer! 10 "test"))
+          :to-equal '(run-with-idle-timer 10 nil (lambda nil "test")))
+    )
   )
--- blood/__tests/blood--test-structs.el
+++ blood/__tests/blood--test-structs.el
@@ -1,176 +1,78 @@
 ;; blood--test.el -*- mode: emacs-lisp; lexical-binding: t; -*-
 ;;
-;; General ERT tests for blood
 ;;
-(require 'ert)
-(require 'f)
+;;
+(require 'buttercup)
 
-(defun blood--test-structs--setup ()
-  (add-to-list 'load-path (f-parent default-directory))
-  (load "blood-structs")
-  )
-
-(ert-deftest blood--test-sanity ()
-  "Tests a basic identifier construction"
-  :tags '(sanity struct)
-  (blood--test-structs--setup)
-  (let ((iden (make-blood--identifier-s :profile 'test)))
-    (should iden)
-    (should (blood--identifier-s-p iden))
-    (should (eq (blood--identifier-s-profile iden) 'test))
+(describe "identifiers"
+  (it "is a sanity test" (expect t :to-be (not nil)))
+  (it "should build"
+    (expect (make-blood--identifier-s :profile 'test) :to-pass #'blood--identifier-s-p)
     )
-)
 
-;; test identifiers
-(ert-deftest blood--test-structs-id-test ()
-  "Tests identifier construction"
-  :tags '(struct)
-  (blood--test-structs--setup)
-  (let* ((iden (make-blood--identifier-s :profile 'test
-                                         :group   'blah
-                                         :module  'bloo
-                                         :package 'basic
-                                         ))
-         )
-    (should-error (blood--id-sym nil :profile t))
-    (should-error (blood--id-sym iden))
+  (it "should be able to construct symbol"
+    (let* ((iden (make-blood--identifier-s :profile 'test :group   'blah :module  'bloo :package 'basic)))
+      (expect (blood--id-sym nil :profile t) :to-raise 'error)
+      (expect (blood--id-sym iden) :to-raise 'error)
 
-    (should (eq (blood--id-sym iden :profile t) 'test))
-    (should (eq (blood--id-sym iden :profile t :group t) 'test:blah))
-    (should (eq (blood--id-sym iden :profile t :group t :module t) 'test:blah:bloo))
-    (should (eq (blood--id-sym iden :profile t :group t :module t :package t) 'test:blah:bloo:basic))
-    (should (eq (blood--id-sym iden :profile t :group nil :module t :package t) 'test:bloo:basic))
-    (should (eq (blood--id-sym iden :profile nil :package nil :group t :module t) 'blah:bloo))
-
+      (expect (blood--id-sym iden :profile t) :to-be 'test)
+      (expect (blood--id-sym iden :profile t :group t) :to-be 'test:blah)
+      (expect (blood--id-sym iden :profile t :group t :module t) :to-be 'test:blah:bloo)
+      (expect (blood--id-sym iden :profile t :group t :module t :package t) :to-be 'test:blah:bloo:basic)
+      (expect (blood--id-sym iden :profile t :group nil :module t :package t) :to-be 'test:bloo:basic)
+      (expect (blood--id-sym iden :profile nil :package nil :group t :module t) :to-be 'blah:bloo)
+      )
     )
-)
-
-(ert-deftest blood--test-structs-id-path-test ()
-  "Tests identifier construction from a path"
-  :tags '(struct)
-  (blood--test-structs--setup)
-  (let ((res (blood-build-id-from-path "simple" "/blah/bloo/blee/example.el")))
-    (should res)
-    (should (blood--identifier-s-p res))
-    (should (blood--identifier-s-source res))
-    (should (eq (blood--identifier-s-group res) 'bloo))
-    (should (eq (blood--identifier-s-module res) 'blee))
-    (should (string-equal (blood--identifier-s-source res) "/blah/bloo/blee/example.el"))
+  (it "should be able to build from a path"
+    (expect (blood-build-id-from-path "simple" "/blah/bloo/blee/example.el") :to-pass #'blood--identifier-s-p)
+    )
+  (it "should record the originating path"
+    (expect (blood--identifier-s-source (blood-build-id-from-path "simple" "/blah/bloo/blee/example.el")) :to-equal "/blah/bloo/blee/example.el")
+    )
   )
-)
 
 ;; test advice
-(ert-deftest blood--test-structs-advice-empty-struct-test ()
-    "Basic Test of empty advice data"
-    :tags '(struct)
-    (blood--test-structs--setup)
-    (let ((res (blood-build-advice '(:targets #'message :advisors nil))))
-      (should-not res)
+(describe "advice"
+  ;; Vars:
+  :var (a)
+  ;; Setup
+  (before-each nil)
+  ;; Teardown
+  (after-each nil)
+  ;; Specs:
+  (it "is a sanity test" (expect t :to-be (not nil)))
+  (it "should build"
+    (expect (blood-build-advice '(:targets #'message :advisors nil) :to-pass #'blood--advice-s-p)))
+  (it "should fail on bad arguments"
+    (expect (blood-build-advice '(:targets #'message :advisors ((:around)))) :to-raise 'error)
+    (expect (blood-build-advice '(:targets #'message :advisors ((:bad-target #'bloo)))) :to-raise 'error)
     )
-)
-
-(ert-deftest blood--test-structs-advice-struct-test ()
-    "Basic Test of advice"
-    :tags '(struct)
-    (blood--test-structs--setup)
-    (let ((res (blood-build-advice '(:targets #'message :advisors ((:around #'blah)))) ))
-      (should res)
-      (should (blood--advice-s-p res))
-    )
-)
-
-(ert-deftest blood--test-structs-advice-fail-struct-test ()
-    "Ensure Advice errors on bad data"
-    :tags '(struct)
-    (blood--test-structs--setup)
-    (should-error (blood-build-advice '(:targets #'message :advisors ((:around)))))
-    (should-error (blood-build-advice '(:targets #'message :advisors ((:bad-target #'bloo)))))
 )
 
 ;; test paths
-(ert-deftest blood--test-structs-paths-test ()
-  "Tests paths collection"
-  :tags '(struct)
-  (blood--test-structs--setup)
-  ;; Error on no modules loc
-  (should-error (blood-build-paths '()))
-  ;; Normal use with defaults
-  (let ((res (blood-build-paths '(:modules "."))))
-    (should (blood--paths-s-p res))
-    (should (string-equal (car (blood--paths-s-modules res)) "."))
-    (should (string-equal (car (blood--paths-s-cache res)) blood--cache-loc-default))
-    (should (string-equal (car (blood--paths-s-install res)) blood--install-loc-default))
-    (should (string-equal (car (blood--paths-s-build res)) blood--build-loc-default))
-    (should (string-equal (car (blood--paths-s-secrets res)) blood--secrets-loc-default))
+(describe "paths"
+  ;; Vars:
+  :var (a)
+  ;; Setup
+  (before-each nil)
+  ;; Teardown
+  (after-each nil)
+  ;; Specs:
+  (it "is a sanity test" (expect t :to-be (not nil)))
+  (it "should error on bad args"
+    (expect (blood-build-paths '()) :to-raise 'error)
     )
-  ;; multi module locations
-  (cl-loop for pair in (-zip-pair (blood--paths-s-modules (blood-build-paths '(:modules ("../first" "../second"))))
-                             (list "../first" "../second"))
-           do (should (string-equal (car pair) (cdr pair)))
-           )
-  ;; Defaults overridden
-  (let ((res (blood-build-paths '(:modules "."))))
-    (should (blood--paths-s-p res))
+  (it "should build"
+    (expect (blood-build-paths '(:modules ".")) :to-pass #'blood--paths-s-p)
     )
-)
-
-
-;; test recipe
-(ert-deftest blood--test-structs-recipe-test ()
-  "Tests basic recipe construction"
-  :tags '(struct)
-  (blood--test-structs--setup)
-  (let ((recipe (blood-build-recipe '()))
-        )
-    (should (not (blood-build-recipe nil)))
-    (should (eq (blood-build-recipe 'melpa) 'melpa))
-    (should recipe)
-    (should (blood--recipe-s recipe))
+  (it "should have default locations"
+    (let ((res (blood-build-paths '(:modules "."))))
+      (expect (car (blood--paths-s-modules res)) :to-equal ".")
+      (expect (car (blood--paths-s-cache res))   :to-equal blood--cache-loc-default)
+      (expect (car (blood--paths-s-install res)) :to-equal blood--install-loc-default)
+      (expect (car (blood--paths-s-build res))   :to-equal blood--build-loc-default)
+      (expect (car (blood--paths-s-secrets res)) :to-equal blood--secrets-loc-default)
+      )
     )
+
 )
-
-;; test constraint
-(ert-deftest blood--test-structs-constraint-test ()
-  "Tests basic activation constraint definition"
-  :tags '(struct)
-  (blood--test-structs--setup)
-  (should (equal 0 1))
-)
-
-;; test package
-(ert-deftest blood--test-structs-package-test ()
-  "Tests package declaraction"
-  :tags '(struct)
-  (blood--test-structs--setup)
-  (let ((package (blood-build-package 'evil "a/group/module/file.el"
-                                      '(:on-init (
-                                                  (message "Test")
-                                                  )
-                                        )
-                                      )
-                 )
-        )
-    (should package)
-    (should (blood--package-s-p package))
-    (should (equal (blood-uniq-id package) 'group:module:evil))
-    (should (functionp (blood--package-s-on-init package)))
-
-    )
-  )
-
-;; test module
-(ert-deftest blood--test-structs-module-test ()
-  "Tests module declaration"
-  :tags '(struct)
-  (blood--test-structs--setup)
-  (should (equal 0 1))
-)
-
-;; test profile building
-(ert-deftest blood--test-structs-profile-test ()
-  "Tests profile declaration"
-  :tags '(struct)
-  (blood--test-structs--setup)
-  (should (equal 0 1))
-)
-
